0de861bc692355c7fd36baec5f6df9fcb04ab314
Kevin Traini <kevin@ktraini.com>
2024-06-11 19:07:27 +0200

    chore: Add Prettier
    Part of #3

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-    build: {
-        lib: {
-            entry: './lib/main.ts',
-            name: 'web-terminal',
-            fileName: 'web-terminal',
-        },
+  build: {
+    lib: {
+      entry: "./lib/main.ts",
+      name: "web-terminal",
+      fileName: "web-terminal",
     },
-    plugins: [dts()],
+  },
+  plugins: [dts()],
 });
